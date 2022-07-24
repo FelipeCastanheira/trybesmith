dc080ffdd936b45d1137839a3a7874d95c12ce43
Felipe Castanheira <castanheirafc2@gmail.com>
2022-07-24 17:34:26 -0300

    token

---
+++ src/utils/token.ts
@@ -0,0 +1,6 @@
+const tokenA = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODk';
+const tokenB = 'wIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.';
+const tokenC = 'SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+const token = `${tokenA}${tokenB}${tokenC}`;
+
+export default token;
